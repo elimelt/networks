4277cbab5658701d5d5c11654d2697617bfe0862
Elijah Melton <elimelt@uw.edu>
2024-01-16 20:56:00 -0800

    worked

--- lab1/client.py
+++ lab1/client.py
@@ -1,11 +1,65 @@
+import socket
 from request import Request, Header
+from struct import pack, unpack
+from math import ceil
 
+def stage_a() -> tuple[int, int, int, int]:
+    req = Request()
+    header = Header(12,0,1)
+    req.add_header(header)
+    req.add_payload(b"hello world")
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    udp_host = "attu2.cs.washington.edu"
+    udp_port = 12235
+
+    msg = req.to_network_bytes()
+    sock.sendto(msg, (udp_host,udp_port))
+
+    response = sock.recv(28)
+    num, len_b, udp_port_a, secretA = unpack('!IIII', response[12:])
+
+    return (num, len_b, udp_port_a, secretA)
+
+def stage_b(num: int, length: int, udp_port: int, secretA: int) -> tuple[int, int]:
+    # byte aligned length by 4
+    len_byte_aligned = int(ceil(length / 4) * 4)
+
+    header = Header(len_byte_aligned, secretA, 1)
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    # socket.settimeout(1)
+    udp_host = "attu2.cs.washington.edu"
+
+    def create_payload_bytes(id, payload, payload_length):
+        return pack(f"!I{payload_length}s", id, payload)
+
+    print(f'sending {num} requests to {udp_port}')
+
+    for packet_id in range(num):
+        req = Request()
+        payload = b'0' * len_byte_aligned
+
+        req.add_header(header)
+        req.add_payload(create_payload_bytes(packet_id, payload, len_byte_aligned))
+        msg = req.to_network_bytes()
+
+        sock.sendto(msg, (udp_host, udp_port))
+        print(f'sending req {packet_id}')
+        ack_response = sock.recv(16)
+        print('done')
+        unpacked_ack_response = unpack('!I', ack_response[12:])
+        print(unpacked_ack_response)
+
+
+    #  TCP port number, secretB.
+    response = sock.recv(20)
+    TCP_port, secretB = unpack('!II', response[12:])
+
+    return TCP_port, secretB
 
 
 if __name__ == '__main__':
-
-    req = Request()\
-        .with_payload([1,2,3,4,5,6,7,8,9,10])\
-        .with_header(Header())
-
-    print(req.to_network_bytes())
+    numB, lenB, udp_port_a, secretA = stage_a()
+    print(f'found port {udp_port_a}')
+    TCP_port, secretB = stage_b(numB, lenB, udp_port_a, secretA)
--- lab1/request.py
+++ lab1/request.py
@@ -1,30 +1,34 @@
-class Header:
-    def __init__(self):
-        # 4 bytes
-        self.payload_len = 0
-        # 4 bytes
-        self.psecret = 0  
-        # 2 bytes
-        self.step = 0
-        # 2 bytes
-        self.sid_checksum = 822
+from struct import pack, unpack
 
+STUDENT_ID = 822
+
+class Header:
+    def __init__(self, payload_len, psecret, step):
+        # 4 bytes
+        self.payload_len = payload_len
+        # 4 bytes
+        self.psecret = psecret
+        # 2 bytes
+        self.step = step
+        # 2 bytes - last 3 digits of Elijah's netid
+        self.sid_checksum = STUDENT_ID
 class Request:
     def __init__(self):
-        self.header = Header()
-        self.payload = []
-        
-    def with_payload(self, payload):
+        self.header: Header = None
+        self.payload: bytes = b""
+
+    # payload is type bytes literal
+    def add_payload(self, payload: bytes):
         self.payload = payload
-        self.header.payload_len = len(payload)
-        return self
+        self.header.payload_len = len(payload) + 1
 
-    def with_header(self, header):
+    def add_header(self, header):
         self.header = header
-        return self
-        
+
     def to_network_bytes(self):
-        print("Request::to_network_bytes not implemented")
-        exit(1)
-
-
+        # ! means network byte order
+        # I means unsigned int
+        # H means unsigned short
+        header_bytes = pack('!IIHH', self.header.payload_len, self.header.psecret, self.header.step, self.header.sid_checksum)
+        payload_bytes = pack(f'!{self.header.payload_len}s', self.payload)
+        return header_bytes + payload_bytes
