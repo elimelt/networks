7df75edb649286d6584c2fd5625942fee210a830
Nathan Khuat <nkhuat@cs.washington.edu>
2024-01-22 13:58:03 -0800

    finished server part a

--- lab1/server/server.py
+++ lab1/server/server.py
@@ -4,7 +4,7 @@ from struct import pack, unpack
 import random
 from response import Response, Header
 
-HOST = "attu3.cs.washington.edu"
+HOST = "attu4.cs.washington.edu"
 PORT = 12235
 STEP = 2
 HEADER_SIZE = 12
@@ -14,11 +14,10 @@ STAGE_A_EXPECTED_PAYLOAD_LEN = 12
 # thread handler
 def handle_client_handshake(req_bytes, sock, client_addr):
     print("connection: ", client_addr)
-    print("starting stage a:")
     status_a = stage_a(req_bytes, sock, client_addr)
-    if status_a:
+    if stage_a:
         print("stage a success.")
-    status_b = stage_b()
+    # status_b = stage_b()
 
 def check_header(header, payload_len, secret, step):
     plen, psecret, step, num = unpack('!IIHH', header)
@@ -40,33 +39,38 @@ def start_server(port):
             sock.close()
 
 def stage_a(req_bytes, sock, client_addr) -> tuple[int]:
-    
+    print(list(req_bytes))
+    print(len(list(req_bytes)))
     header = req_bytes[:HEADER_SIZE]
     payload = req_bytes[HEADER_SIZE:]
     if not check_header(header, STAGE_A_EXPECTED_PAYLOAD_LEN, 0, 1):
         return None
-
+    
+    print("header:", list(header))
+    print("payload:", list(payload))
     # if len(payload) != STAGE_A_EXPECTED_PAYLOAD_LEN:
-    #     return None
+    #    return None
 
     payload_str, = unpack(f'!{STAGE_A_EXPECTED_PAYLOAD_LEN}s', payload)
-    print("unpacked string", payload_str)
+    payload_str = payload_str.decode('utf-8').strip('\x00')
+    print("payload_str:", payload_str)
+    print("hello world", payload_str == "hello world")
     if payload_str != "hello world":
         return None
-
+    print("got here")
     # num, len, udp_port, secretA
     output_num = random.randint(5, 30)
     output_len = random.randint(15, 50)
     output_port = random.randint(60000, 65000)
     output_secret = random.randint(1, 100)
 
-    payload, = pack("!IIII", output_num, output_len, output_port, output_secret)
-    response = Response(Header(16, 0, 1), payload)
+    payload = pack("!IIII", output_num, output_len, output_port, output_secret)
+    response = Response(Header(16, 0, 2), payload)
 
     msg = response.to_network_bytes()
 
     sock.sendto(msg, client_addr)
-    print("sent message A to", client_addr)
+    print("sent message A to: ", client_addr)
     return (output_num, output_len, output_port, output_secret)
 
 def stage_b(udp_port: int, expected_len) -> bool:
